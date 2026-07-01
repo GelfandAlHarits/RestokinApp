2c10cf97645fa8b653a31796be44b969264b8108
GelfandAlHarits <gelfandalharitszakaria@gmail.com>
2026-07-01 15:09:41 +0700

    bismillah4

--- api/index.php
+++ api/index.php
@@ -1,3 +1,21 @@
 <?php
 
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+register_shutdown_function(function () {
+    $e = error_get_last();
+
+    if ($e) {
+        file_put_contents(
+            '/tmp/php-fatal.log',
+            print_r($e, true),
+            FILE_APPEND
+        );
+    }
+});
+
+echo "Reached api/index.php\n";
+flush();
+
 require __DIR__.'/../public/index.php';
